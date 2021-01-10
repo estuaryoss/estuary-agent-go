599ecd8d2349a47e6b0d2df9f4f2fcc23829cc7c
Catalin Dinuta <constantin.dinuta@gmail.com>
2021-01-10 10:34:46 +0200

    added extra logging boot

--- Main.go
+++ Main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/estuaryoss/estuary-agent-go/src/constants"
@@ -11,6 +12,12 @@ import (
 func main() {
 	appPort := environment.GetInstance().GetConfigEnvVars()[constants.PORT]
 
-	log.Printf("Running on port %s\n", appPort)
+	log.Printf("Running on port: %s\n\n", appPort)
+	configEnvVars, _ := json.Marshal(environment.GetInstance().GetConfigEnvVars())
+	log.Printf("Config env vars: %s\n\n", configEnvVars)
+	systemEnvVars, _ := json.Marshal(environment.GetInstance().GetEnvAndVirtualEnv())
+	log.Printf("Environment: %s\n\n", systemEnvVars)
+	userEnvVars, _ := json.Marshal(environment.GetInstance().GetVirtualEnv())
+	log.Printf("User(virtual) Environment: %s\n\n", userEnvVars)
 	routes.SetupServer(appPort)
 }
